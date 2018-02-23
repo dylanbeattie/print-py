9e161977ca78f2ab1dd308534bcd52baa35173bf
dylanbeattie <dylan@dylanbeattie.net>
2018-02-23 17:21:06 +0000

    Proof of concept downloading badges from remote API

--- app.py
+++ app.py
@@ -3,14 +3,25 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
+import shutil
+
+import requests
+
+url = 'http://example.com/img.png'
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
         name = request.form['name']
-        os.system("convert -background white -fill black -size 720x300 -pointsize 128 -gravity center label:\"{0}\" output.png".format(name))
-        os.system("/home/pi/.local/bin/brother_ql_create --model QL-720NW ./output.png > output.bin")
+        url = "http://192.168.10.181/home/label/" + name
+        response = requests.get(url, stream=True)
+        with open('output.png', 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        del response
+        # os.system("convert -background white -fill black -size 720x300 -pointsize 128 -gravity center label:\"{0}\" output.png".format(name))
+        os.system("~/.local/bin/brother_ql_create --model QL-720NW ./output.png > output.bin")
         os.system("cat output.bin > /dev/usb/lp0")
         return 'Printed!'
     else:
